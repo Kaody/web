75d28095aba938d73d77cdae912462b38ed06594
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2024-02-23 12:21:23 +0300

    build: rewrite the incomming request path to a different destination path
    rewrites act as a URL proxy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    const BASE_URL = process.env.API_BASE_URL;
+    const API_PATH = process.env.NEXT_PUBLIC_API_PATH;
+    const GRAPHQL_PATH = process.env.NEXT_PUBLIC_GRAPHQL_PATH;
+    return [
+      {
+        source: `${API_PATH}/:path*`,
+        destination: `${BASE_URL}${API_PATH}/:path*`,
+      },
+      {
+        source: `${GRAPHQL_PATH}/:path*`,
+        destination: `${BASE_URL}${GRAPHQL_PATH}/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
