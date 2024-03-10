fd61e357a78e05073d7f2c2d8390ce72a75bca56
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2024-03-10 23:55:14 +0300

    fix(app): remove dynamic constant in the home page component

--- src/app/(market)/page.tsx
+++ src/app/(market)/page.tsx
@@ -4,8 +4,6 @@ import { News } from "@/components/products/news";
 import { Promo } from "@/components/products/promo";
 import { Recommended } from "@/components/products/recommended";
 
-export const dynamic = false;
-
 export default async function Home() {
   return (
     <main>
