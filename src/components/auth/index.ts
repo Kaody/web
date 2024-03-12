10dee708a5948053500a8f8bd8a78c06caf6b719
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2024-03-12 14:54:38 +0300

    feat(components): export auth components

---
+++ src/components/auth/index.ts
@@ -0,0 +1,5 @@
+import LoginForm from "@/components/auth/login.form";
+import RegisterForm from "@/components/auth/register.form";
+import { Container } from "./container/Container";
+
+export { Container, LoginForm, RegisterForm };
