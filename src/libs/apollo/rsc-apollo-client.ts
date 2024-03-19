020a91eea65007366c6ae13c382bfa1883f396be
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2024-03-19 13:32:09 +0300

    feat(lib:apollo): remove authLink from rsc apollo client

--- src/libs/apollo/rsc-apollo-client.ts
+++ src/libs/apollo/rsc-apollo-client.ts
@@ -1,10 +1,10 @@
 import { ApolloClient, InMemoryCache, from } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
-import { authLink, errorLink } from "@/libs/apollo/links";
+import { errorLink } from "@/libs/apollo/links";
 import { rscHttpLink } from "@/libs/apollo/links/rsc.link";
 
-const link = from([errorLink, authLink, rscHttpLink]);
+const link = from([errorLink, rscHttpLink]);
 
 const client = new ApolloClient({
   link,
