38f0fba86463df6bc1c51ee1c00cfb4fa468db94
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2024-03-19 14:18:14 +0300

    feat(app): wrappe news, promo, flash and recommended component with suspense in home page

--- src/app/(market)/page.tsx
+++ src/app/(market)/page.tsx
@@ -1,8 +1,13 @@
+import { Skeleton } from "@mantine/core";
+import { Suspense } from "react";
+
 import { Hero } from "@/components/hero";
 import { Flash } from "@/components/products/flash";
 import { News } from "@/components/products/news";
 import { Promo } from "@/components/products/promo";
 import { Recommended } from "@/components/products/recommended";
+
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   return (
@@ -12,10 +17,18 @@ export default async function Home() {
       }}
     >
       <Hero />
-      <News />
-      <Promo />
-      <Flash />
-      <Recommended />
+      <Suspense fallback={<Skeleton mt="xl" height={50} width="100%" />}>
+        <News />
+      </Suspense>
+      <Suspense fallback={<Skeleton mt="xl" height={50} width="100%" />}>
+        <Promo />
+      </Suspense>
+      <Suspense fallback={<Skeleton mt="xl" height={50} width="100%" />}>
+        <Flash />
+      </Suspense>
+      <Suspense fallback={<Skeleton mt="xl" height={50} width="100%" />}>
+        <Recommended />
+      </Suspense>
     </main>
   );
 }
